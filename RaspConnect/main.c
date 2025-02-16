d113d42decfc585721deb9ad4a520b3328f70bad
SWFD <swfd@outlook.jp>
2025-02-16 15:16:22 +0100

    Initial Commit

---
+++ RaspConnect/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  RaspConnect
+//
+//  Created by Emran Sediq Sultani on 16.02.25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
